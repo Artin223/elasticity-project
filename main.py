a3a6949581f86ac48c38b5d21ceffc9384656ae7
Artin223 <shirzadiartin06@gmail.com>
2026-04-20 23:42:51 -0700

    Completed the Infrastructure

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import pandas as pd
+import os
+from dotenv import load_dotenv
+from fredapi import Fred
+load_dotenv("APIFredKey.env")
+FredAPIKey= os.getenv("APIFredKey")
+FredReady = Fred(api_key=FredAPIKey)
+
+IDChoice = input("Enter your object of interest: ")
 
