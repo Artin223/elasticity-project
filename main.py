9a985f1e37b4d78ec410bcf576cecd1c1b8f8463
Artin223 <shirzadiartin06@gmail.com>
2026-05-05 21:53:17 -0700

    Calculate and display price elasticity based on user-defined time range

--- main.py
+++ main.py
@@ -27,4 +27,18 @@ DemandSeries = FredActivate.get_series(DemandID)
 MonthlyPriceSeries = PriceSeries.resample('MS').mean()
 MonthlyDemandSeries = DemandSeries.resample('MS').mean()
 
-print(MonthlyPriceSeries, MonthlyDemandSeries)
+if StartTimeChoice != "latest":
+    Price1 = MonthlyPriceSeries.loc[StartTimeChoice].iloc[0]
+    Price2 = MonthlyPriceSeries.loc[EndTimeChoice].iloc[0]
+    Demand1 = MonthlyDemandSeries.loc[StartTimeChoice].iloc[0]
+    Demand2 = MonthlyDemandSeries.loc[EndTimeChoice].iloc[0]
+else:
+    Price1 = MonthlyPriceSeries.iloc[-2]
+    Price2 = MonthlyPriceSeries.iloc[-1]
+    Demand1 = MonthlyDemandSeries.iloc[-2]
+    Demand2 = MonthlyDemandSeries.iloc[-1]
+
+PriceElasticity = ((Demand2 - Demand1) / Demand1) / ((Price2 - Price1) / Price1)
+PriceElasticity = abs(PriceElasticity)
+PriceElasticity = round(PriceElasticity, 2)
+print(f"The price elasticity of {IDChoice} is: {PriceElasticity}")
